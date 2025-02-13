08d682667fdb1715e65c5f9eeeb87779a5a3ae51
Davi Taveira <devtaveira@gmail.com>
2025-02-13 09:23:52 -0300

    queries for pdv and estacoes created

--- clientes_sg/subquery-produtos.sql
+++
@@ -1,9 +0,0 @@
--- CONSTRUIR SUBQUERY PRODUTOS DINAMICAMENTE
-select
-	concat( 'select cnpj as CNPJPRODUTO, ', group_concat( 
-CONCAT(
-    "
-IF( prodsg.produto = ", p.produto, ", 'OK', '') AS '", UPPER(p.descricao), "'"
-  )), ' from prodsg group by cnpj' )
-from
-	produtos p
